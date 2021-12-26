57f93df46124c1f27486831b022c421ab3548e4a
xsj <362372538@qq.com>
2021-12-26 20:39:53 +0800

    init login demo

---
+++ Example/Pods/Target Support Files/XSJZjhTestLogin/XSJZjhTestLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XSJZjhTestLogin : NSObject
+@end
+@implementation PodsDummy_XSJZjhTestLogin
+@end
---
+++ XSJZjhTestLogin/Classes/XSJLogin.swift
@@ -0,0 +1,13 @@
+//
+//  XSJLogin.swift
+//  XSJZjhTestLogin_Example
+//
+//  Created by 肖硕杰 on 2021/12/26.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class XSJLogin: NSObject {
+
+}
